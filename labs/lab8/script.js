6d615ac0ff3e8d93cef6e6ac1d60a697b3ec22f9
Bryan Aguirre <baguirre2651@berkeley.edu>
2023-04-27 16:13:39 -0700

    ye

--- labs/lab8/script.js
+++
@@ -1,8 +0,0 @@
-function toggleTheme() {
-    var body = document.querySelector('body');
-    body.classList.toggle('dark-mode');
-  }
-  
-  var buttonVariable = document.getElementById('toggleButton');
-  buttonVariable.onclick = toggleTheme;
-  
